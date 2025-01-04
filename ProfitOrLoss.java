adc8be66cb38b310f8b3c53900b0061e39732527
Priyanshi Tomar <168635744+Priyanshitomar@users.noreply.github.com>
2025-01-04 14:20:44 +0530

    Create  ProfitOrLoss.java

---
+++ ProfitOrLoss.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class ProfitOrLoss {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter cost price: ");
+        double costPrice = scanner.nextDouble();
+        System.out.print("Enter selling price: ");
+        double sellingPrice = scanner.nextDouble();
+
+        if (sellingPrice > costPrice) {
+            System.out.println("Profit: " + (sellingPrice - costPrice));
+        } else if (costPrice > sellingPrice) {
+            System.out.println("Loss: " + (costPrice - sellingPrice));
+        } else {
+            System.out.println("No profit, no loss.");
+        }
+    }
+}
