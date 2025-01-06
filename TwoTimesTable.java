131b5347b3c5d1036ab71ac10f0ec5175602ae44
Priyanshi Tomar <168635744+Priyanshitomar@users.noreply.github.com>
2025-01-06 16:07:08 +0530

    TwoTimesTable.java

---
+++ TwoTimesTable.java
@@ -0,0 +1,7 @@
+public class TwoTimesTable {
+    public static void main(String[] args) {
+        for (int i = 0; i <= 10; i++) {
+            System.out.println("2 x " + i + " = " + (2 * i));
+        }
+    }
+}
