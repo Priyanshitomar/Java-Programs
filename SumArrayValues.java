ff69194ee85f5a393dfbadbae85fe049dd0b17ce
Priyanshi Tomar <168635744+Priyanshitomar@users.noreply.github.com>
2025-01-10 22:26:22 +0530

    SumArrayValues.java

---
+++ SumArrayValues.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class SumArrayValues {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the size of the array: ");
+        int size = scanner.nextInt();
+
+        int[] array = new int[size];
+        System.out.println("Enter elements of the array:");
+        int sum = 0;
+
+        for (int i = 0; i < size; i++) {
+            array[i] = scanner.nextInt();
+            sum += array[i];
+        }
+
+        System.out.println("Sum of array values: " + sum);
+    }
+}
